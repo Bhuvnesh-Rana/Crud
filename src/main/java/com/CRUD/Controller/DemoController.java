dcb87030d60352d38feb7e7d44c637b03c6ab867
Bhuvnesh-Rana <bhuvnesh.rana28@gmail.com>
2024-05-05 21:18:20 +0530

    Updated controller post

--- src/main/java/com/CRUD/Controller/DemoController.java
+++ src/main/java/com/CRUD/Controller/DemoController.java
@@ -3,6 +3,9 @@ package com.CRUD.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,8 +29,8 @@ public class DemoController {
     }
 
     @PostMapping("demo")
-    public DemoDTO addDemoUser(@RequestBody DemoDTO demoDTO){
-        return demoService.addDemoUser(demoDTO);
+    public ResponseEntity<DemoDTO> addDemoUser(@RequestBody DemoDTO demoDTO){
+        return new ResponseEntity<DemoDTO>(demoService.addDemoUser(demoDTO), HttpStatus.CREATED);
     }
 
     @GetMapping("demo")
