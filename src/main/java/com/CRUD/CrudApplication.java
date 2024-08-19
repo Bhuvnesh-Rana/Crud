718e00e5725d7234afb0d6adb1754f18174dd757
Bhuvnesh-Rana <bhuvnesh.rana28@gmail.com>
2024-08-19 20:57:28 +0530

    Check jasyspt may cause error.

--- src/main/java/com/CRUD/CrudApplication.java
+++ src/main/java/com/CRUD/CrudApplication.java
@@ -3,7 +3,10 @@ package com.CRUD;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class CrudApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/CRUD/CrudApplicationTests.java
+++ src/test/java/com/CRUD/CrudApplicationTests.java
@@ -1,13 +1,33 @@
 package com.CRUD;
 
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CrudApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void testEncryption(){
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword("password");
+		config.setAlgorithm("PBEWithMD5AndDES");
+		config.setKeyObtentionIterations("1000");
+		config.setPoolSize("1");
+		config.setProviderName("SunJCE");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setStringOutputType("base64");
+		encryptor.setConfig(config);
+
+		String t = "root";
+		System.out.println("hahahaah");
+		System.out.println("Encypted value: "+encryptor.encrypt(t));
+
 	}
 
 }
